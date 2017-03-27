1152df5dbe450e2b67f416c92cb8dd35a810d960
Tapopadma Tripathy <tapopadmatripathy1995@gmail.com>
2017-03-27 09:24:31 +0800

    Update KMP.cpp

--- KMP.cpp
+++ KMP.cpp
@@ -3,14 +3,11 @@
 using namespace std;
 
 void getLPS(int *lps, string P){
-	// length of the previous longest prefix suffix
 	int len = 0;
 
 	int M = P.length();
 
-	lps[0] = 0; // lps[0] is always 0
-
-	// the loop calculates lps[i] for i = 1 to M-1
+	lps[0] = 0; 
 	int i = 1;
 	while (i < M){
 		if (P[i] == P[len]){
@@ -18,17 +15,11 @@ void getLPS(int *lps, string P){
 			lps[i] = len;
 			i++;
 		}
-		else { // (P[i] != P[len])
-			// This is tricky. Consider the example.
-			// AAACAAAA and i = 7. The idea is similar 
-			// to search step.
+		else { 
 			if (len != 0){
 				len = lps[len - 1];
-
-				// Also, note that we do not increment
-				// i here
 			}
-			else // if (len == 0)
+			else
 			{
 				lps[i] = 0;
 				i++;
@@ -44,8 +35,8 @@ void KMP::testKMP(){
 	getLPS(lps, P);
 	int n = T.length();
 	int m = P.length();
-	int i = 0;  // index for txt[]
-	int j = 0;  // index for pat[]
+	int i = 0;  
+	int j = 0;  
 	while (i < n)
 	{
 		if (P[j] == T[i])
@@ -60,11 +51,8 @@ void KMP::testKMP(){
 			j = lps[j - 1];
 		}
 
-		// mismatch after j matches
 		else if (i < n && P[j] != T[i])
 		{
-			// Do not match lps[0..lps[j-1]] characters,
-			// they will match anyway
 			if (j != 0)
 				j = lps[j - 1];
 			else
