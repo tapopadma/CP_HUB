ea098cff8e77840f69b98da5f9ec5336d710969c
tapopadma <tapopadmatripathy1995@gmail.com>
2017-04-08 09:21:34 +0800

    treap

--- Treap.cpp
+++ Treap.cpp
@@ -1,16 +1,92 @@
 #include "Treap.h"
 
-Treap::Treap(){
-	root = NULL;
+void Treap::testTreap(){
+	srand(time(NULL));//to generate unique sequence of random numbers every time code is run
+	Treap t;
+	t.insert(50); t.insert(30); t.insert(20);
+	t.insert(40); t.insert(70); t.insert(60); t.insert(80);
+
+	cout << "Inorder traversal of the given tree \n";
+	t.print();
+
+	cout << "\nDelete 20\n"; t.erase(20);
+	cout << "Inorder traversal of the modified tree \n";
+	t.print();
+
+	cout << "\nDelete 30\n"; t.erase(30);
+	cout << "Inorder traversal of the modified tree \n";
+	t.print();
+
+	cout << "\nDelete 50\n"; t.erase(50);
+	cout << "Inorder traversal of the modified tree \n";
+	t.print(); t.search(50);
+}
+
+void Treap::inorder(TreapNode* root){
+	if (root){
+		inorder(root->left);
+		cout << "key: " << root->key << " | priority: %d "
+			<< root->priority;
+		if (root->left)
+			cout << " | left child: " << root->left->key;
+		if (root->right)
+			cout << " | right child: " << root->right->key;
+		cout << endl;
+		inorder(root->right);
+	}
+}
+
+void Treap::print(){
+	inorder(root);
+}
+
+void Treap::search(int key){
+	cout << (_search(root, key) == NULL ? "-" : "+");
+}
+
+TreapNode* Treap::_search(TreapNode* root, int key){
+	if (root == NULL || root->key == key)
+		return root;
+	if (root->key < key)
+		return _search(root->right, key);
+	return _search(root->left, key);
+}
+
+TreapNode * Treap::_erase(TreapNode * root, int key){
+	if (root == NULL)
+		return root;
+	if (key < root->key)
+		root->left = _erase(root->left, key);
+	else if (key > root->key)
+		root->right = _erase(root->right, key);
+	else if (root->left == NULL){
+		TreapNode *temp = root->right; delete root; root = temp;
+	}
+	else if (root->right == NULL){
+		TreapNode *temp = root->left; delete root; root = temp;
+	}
+	else if (root->left->priority < root->right->priority){
+		root = leftRotate(root);
+		root->left = _erase(root->left, key);
+	}
+	else{
+		root = rightRotate(root);
+		root->right = _erase(root->right, key);
+	}
+	return root;
+}
+
+void Treap::erase(int key){
+	root = _erase(root, key);
 }
 
 void Treap::insert(int key){
 	root = _insert(root, key);
 }
 
-Node * Treap::_insert(Node * root, int key){
+TreapNode * Treap::_insert(TreapNode * root, int key){
 	if(root == NULL){
-		return Node(key);
+		return new TreapNode(key);
 	}
 	if(root->key >= key){
 		root->left = _insert(root->left, key);
@@ -19,13 +95,28 @@ Node * Treap::_insert(Node * root, int key){
 		root->right = _insert(root->right, key);
 	}
 	root = balance(root);
+	return root;
 }
 
-Node * Treap::balance(Node * root){
+TreapNode * Treap::balance(TreapNode * root){
 	if(root->left != NULL && root->left->priority > root->priority){
 		root = rightRotate(root);
 	}
 	if(root->right != NULL && root->right->priority > root->priority){
 		root = leftRotate(root);
 	}
+	return root;
+}
+
+TreapNode * Treap::leftRotate(TreapNode *x){
+	TreapNode *y = x->right, *T2 = y->left;
+	y->left = x;
+	x->right = T2;
+	return y;
+}
+
+TreapNode *Treap::rightRotate(TreapNode *y){
+	TreapNode * x = y->left, *T2 = x->right;
+	x->right = y; y->left = T2;
+	return x;
 }
--- Treap.h
+++ Treap.h
@@ -3,27 +3,36 @@ using namespace std;
 
 const int MX = 1e9;
 
-class Node{
+class TreapNode{
 public:
 	int key, priority;
-	Node *left, *right;
-	Node(){
+	TreapNode *left, *right;
+	TreapNode(){
 	}
-	Node(int _key){
+	TreapNode(int _key){
 		key = _key;
 		priority = rand()%MX;
 		left = right = NULL;
 	}
 };
 
-class Treap:public Node{
+class Treap:public TreapNode{
 private:
-	Node *root;
-	Node * _insert(Node *, int);
-	Node * balance(Node *);
+	TreapNode *root;
+	TreapNode * _insert(TreapNode *, int);
+	TreapNode * balance(TreapNode *);
+	TreapNode * _erase(TreapNode *, int);
+	TreapNode * _search(TreapNode *, int);
+	void inorder(TreapNode *);
+	TreapNode * leftRotate(TreapNode*);
+	TreapNode * rightRotate(TreapNode*);
 public:
-	Treap();
+	Treap(){
+		root = NULL;
+	}
+	void search(int);
 	void insert(int);
 	void erase(int);
 	void print();
+	void testTreap();
 };
